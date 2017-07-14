4d845acee7e9b5251c178f8a78935bd8090818ac
Fernando Herrera <fernando@ciedigital.com>
2017-07-14 15:03:31 -0700

    filtered sessons by day

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="movie-list">
       <div v-if="filteredMovies.length">
-        <movie-item v-for="movie in filteredMovies" :movie="movie.movie" :sessions="movie.sessions"></movie-item>
+        <movie-item v-for="movie in filteredMovies" 
+                    :movie="movie.movie" 
+                    :sessions="movie.sessions" 
+                    :day="day"
+                    >
+        </movie-item>
       </div>
       <div v-else-if="movies.length" class="no-results">
           No results
@@ -16,7 +21,7 @@
 import genres from '../util/genres';
 import MovieItem from './MovieItem.vue';
 export default {
-    props: ['genre', 'time', 'movies'],
+    props: ['genre', 'time', 'movies', 'day'],
     methods: {
         moviePassesGenreFilter(movie) {
             if (!this.genre.length) {
